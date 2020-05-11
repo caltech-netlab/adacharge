6df2b7d95ce75d1edc2634dc92f81d7d9d7ebdcb
zach401 <zach401@gmail.com>
2020-05-11 11:02:57 -0700

    Add difference based reallocation.

--- adacharge/postprocessing.py
+++ adacharge/postprocessing.py
@@ -118,7 +118,8 @@ def project_into_discrete_feasible_pilots(rates: np.ndarray,
 def index_based_reallocation(rates: np.ndarray,
                              active_sessions: List[SessionInfo],
                              infrastructure: InfrastructureInfo,
-                             peak_limit: float, sort_fn,
+                             peak_limit: float,
+                             sort_fn,
                              interface: Interface):
     """ Reallocate capacity for first control period up to peak_limit by
         incrementing the pilot signal to each EV after sorting by index_fn.
@@ -131,6 +132,8 @@ def index_based_reallocation(rates: np.ndarray,
         peak_limit (float):
         sort_fn: Function which takes in a SessionInfo object and returns the
             value of the metric.
+        interface (Interface): Interface to information about the environment.
+
 
     Returns:
         np.ndarray: Schedule of charging rates with reallocation up to peak_limit during the first control period.
@@ -168,3 +171,68 @@ def index_based_reallocation(rates: np.ndarray,
             else:
                 active[i] = False
     return rates
+
+
+def diff_based_reallocation(rates: np.ndarray,
+                             active_sessions: List[SessionInfo],
+                             infrastructure: InfrastructureInfo,
+                             interface: Interface):
+    """ Reallocate capacity for first control period by incrementing the
+    pilot signal to each EV. Ordering is determined by the difference between
+    the originally allocated current and the quantized current.
+
+    Args:
+        rates (np.ndarray): Schedule of charging rates.
+        active_sessions (List[SessionInfo]): List of active charging sessions.
+        infrastructure (InfrastructureInfo): Description of the charging
+            infrastructure.
+        interface (Interface): Interface to information about the environment.
+
+
+    Returns:
+        np.ndarray: Schedule of charging rates with reallocation.
+    """
+    init_rates = rates[:, 0]
+    peak_limit = init_rates.sum()
+    rounded_rates = project_into_discrete_feasible_pilots(rates,
+                                                          infrastructure)
+
+    def metric(session):
+        i = infrastructure.get_station_index(session.station_id)
+        return -(init_rates[i] - rounded_rates[i, 0])
+
+    sorted_sessions = sorted(active_sessions, key=metric)
+    sorted_indexes = [infrastructure.get_station_index(s.station_id)
+                      for s in sorted_sessions]
+    active = np.zeros(infrastructure.num_stations, dtype=bool)
+    ub = np.zeros(infrastructure.num_stations)
+    for session in active_sessions:
+        # Do not record energy demands for sessions not active in the first
+        # time interval, as these could be future sessions at the same station.
+        if session.arrival_offset == 0:
+            i = infrastructure.station_ids.index(session.station_id)
+            active[i] = True
+            ub[i] = min([interface.remaining_amp_periods(session),
+                         session.max_rates[0],
+                         infrastructure.max_pilot[i]])
+
+    for i in cycle(sorted_indexes):
+        if not np.any(active):
+            break
+
+        if active[i]:
+            if rounded_rates[i, 0] >= ub[i]:
+                active[i] = False
+                continue
+            new_rates = deepcopy(rounded_rates[:, 0])
+            new_rates[i] = increment_in_set(rounded_rates[i, 0],
+                                            infrastructure.allowable_pilots[i])
+            if np.sum(new_rates) <= peak_limit and \
+                    new_rates[i] <= ub[i] and \
+                    infrastructure_constraints_feasible(new_rates,
+                                                        infrastructure):
+                rounded_rates[:, 0] = new_rates
+            else:
+                active[i] = False
+    print(f'Diff: {peak_limit - rounded_rates[:, 0].sum()}')
+    return rounded_rates
